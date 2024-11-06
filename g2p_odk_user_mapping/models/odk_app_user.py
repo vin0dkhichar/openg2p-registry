0507eaf4746a7bfc80af99848bf50cbe50a410bf
klbtheprogrammer <kalab.rened@outlook.com>
2024-11-06 13:38:58 +0300

    changes on odk user mapper

--- g2p_odk_user_mapping/models/odk_app_user.py
+++ g2p_odk_user_mapping/models/odk_app_user.py
@@ -8,3 +8,5 @@ class ODKAppUser(models.Model):
     name = fields.Char(string="ODK App User Name")
     odk_user_id = fields.Integer(string="ODK App User ID")
     partner_id = fields.Many2one("res.partner", string="Partner")
+    project_id = fields.Char(string="Project", required=True)
+    active = fields.Boolean(default=True)
