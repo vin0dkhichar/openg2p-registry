6ed596911a9b3b75b3fa5b3d8b7e0e41978bac69
joshua160miller <abbb61667@gmail.com>
2024-12-17 12:36:24 +0530

    Added encryption and decryption in documents

--- g2p_registry_documents/models/document_file.py
+++ g2p_registry_documents/models/document_file.py
@@ -1,10 +1,23 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class G2PDocumentRegistry(models.Model):
     _inherit = "storage.file"
 
     registrant_id = fields.Many2one("res.partner")
+
+    is_encrypted = fields.Boolean(string="Encrypted", default=False)
+
+    @api.model
+    def create(self, vals):
+        is_encrypt_fields = (
+            self.env["ir.config_parameter"]
+            .sudo()
+            .get_param("g2p_registry_encryption.encrypt_registry", default=False)
+        )
+        if is_encrypt_fields:
+            vals["is_encrypted"] = True
+        return super().create(vals)
 
     def get_record(self):
         for record in self:
